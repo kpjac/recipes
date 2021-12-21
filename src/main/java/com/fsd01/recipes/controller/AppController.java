8f63949f835970156d7f17aa0bb2cefc331cae40
kpjac <6174409@johnabbottcollege.net>
2021-12-20 20:59:59 -0500

    Login display error

--- src/main/java/com/fsd01/recipes/controller/AppController.java
+++ src/main/java/com/fsd01/recipes/controller/AppController.java
@@ -8,7 +8,6 @@ import com.fsd01.recipes.repository.RecipeMadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
